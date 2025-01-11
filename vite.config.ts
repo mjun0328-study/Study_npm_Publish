854e1f35692f09a5255390f78d91cfba9e040079
Kim Minjun <me@etimel.dev>
2025-01-11 16:23:37 +0900

    Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: resolve(__dirname, "src/index.ts"),
       name: "study-npm-publish",
       formats: ["es", "umd"],
-      fileName: (format) => `study-npm-publish.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
